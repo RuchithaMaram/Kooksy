e38e524b97efd2856500c786f89b17a65fd40210
vaishnavi-chowdary <kommivaishnavi2406@gmail.com>
2024-10-09 12:49:41 -0400

    Updated authentication screen ids and names

--- app/src/main/java/com/teamfour/kooksy/ui/authentication/ForgotPassword.kt
+++ app/src/main/java/com/teamfour/kooksy/ui/authentication/ForgotPassword.kt
@@ -12,7 +12,7 @@ class ForgotPassword : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_forgot_password)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.signuppage)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
--- app/src/main/java/com/teamfour/kooksy/ui/authentication/LandingPage.kt
+++ app/src/main/java/com/teamfour/kooksy/ui/authentication/LandingPage.kt
@@ -8,11 +8,13 @@ import androidx.core.view.WindowInsetsCompat
 import com.teamfour.kooksy.R
 
 class LandingPage : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_landing_page)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.signuppage)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
