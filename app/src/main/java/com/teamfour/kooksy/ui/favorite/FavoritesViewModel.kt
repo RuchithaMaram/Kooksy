69f0b6c182995580caab6463746c9355072bb187
RuchithaMaram <rmara7@unh.newhaven.edu>
2024-10-10 00:15:58 -0400

    "Favorites screen functionality inProgress"
    This reverts commit df26a665f6b8cf2fc3120be8c22a34fe839b6d95.

--- app/src/main/java/com/teamfour/kooksy/ui/favorite/FavoritesViewModel.kt
+++ app/src/main/java/com/teamfour/kooksy/ui/favorite/FavoritesViewModel.kt
@@ -3,6 +3,7 @@ package com.teamfour.kooksy.ui.create
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.teamfour.kooksy.ui.favorite.data.MenuItem
 
 class FavoritesViewModel : ViewModel() {
 
@@ -10,4 +11,36 @@ class FavoritesViewModel : ViewModel() {
         value = "This is Favorites Fragment"
     }
     val text: LiveData<String> = _text
+
+    private val _menuItems = MutableLiveData<List<MenuItem>>()
+    val menuItems: LiveData<List<MenuItem>> = _menuItems
+
+
+    fun getMenuItems() {
+        _menuItems.value = getTempMenuList()
+    }
+
+    private fun getTempMenuList(): List<MenuItem> {
+        val item1 = MenuItem(
+            0,
+            "https://bigoven-res.cloudinary.com/image/upload/d_recipe-no-image.jpg/lowfat-vegetable-lasagna-1336994.jpg",
+            "Vegetable Lasagna",
+            "25 mins",
+            false
+        )
+        val item2 = MenuItem(
+            0,
+            "https://bigoven-res.cloudinary.com/image/upload/f_auto,q_auto/sweetandsourstickythaiboneless-3a944d.jpg",
+            "Sweet and Sour Thai",
+            "20 mins",
+            false
+        )
+
+        return ArrayList<MenuItem>().apply {
+            for (i in 0..10) {
+                add(item1)
+                add(item2)
+            }
+        } as List<MenuItem>
+    }
 }
---
+++ app/src/main/java/com/teamfour/kooksy/ui/favorite/FavouriteAdapter.kt
@@ -0,0 +1,40 @@
+package com.teamfour.kooksy.ui.favorite
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import com.teamfour.kooksy.R
+import com.teamfour.kooksy.ui.favorite.data.MenuItem
+
+class FavouriteAdapter(private val menuItems: List<MenuItem>, onClick: () -> Unit) : RecyclerView.Adapter<FavouriteAdapter.MyViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_favourite, parent, false)
+        return MyViewHolder(view)
+    }
+
+    override fun getItemCount() = menuItems.size
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val menuItem = menuItems.get(position)
+        holder.name.text = menuItem.name
+        holder.time.text= menuItem.time
+        Picasso.get()
+            .load(menuItem.image)
+            .placeholder(R.drawable.ic_favorite_black_24dp)
+            .into(holder.image)
+        holder.favIcon.setImageResource(R.drawable.ic_fav_unselected)
+    }
+
+    class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val image: ImageView = view.findViewById(R.id.item_image)
+        val name: TextView = view.findViewById(R.id.item_name)
+        val time: TextView = view.findViewById(R.id.time_tv)
+        val favIcon: ImageView = view.findViewById(R.id.favourite_iv)
+    }
+}
