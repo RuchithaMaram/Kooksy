2cf1a8d344a3acbb230a76e73ddf2b6949d6c637
vaishnavi-chowdary <kommivaishnavi2406@gmail.com>
2024-10-13 20:39:26 -0400

    Landing page navigations to login and sign up screens done

--- app/src/main/java/com/teamfour/kooksy/ui/authentication/LandingPage.kt
+++ app/src/main/java/com/teamfour/kooksy/ui/authentication/LandingPage.kt
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.teamfour.kooksy.R
+import android.content.Intent
+import android.widget.Button
 
 class LandingPage : AppCompatActivity() {
 
@@ -14,10 +16,27 @@ class LandingPage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_landing_page)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.signuppage)) { v, insets ->
+
+        val loginButton = findViewById<Button>(R.id.LandingLogin)
+        val signupButton = findViewById<Button>(R.id.landingsignup)
+
+        //Navigate to login page
+
+        loginButton.setOnClickListener{
+            val intent = Intent(this,LoginPage::class.java)
+            startActivity(intent)
+        }
+
+        //Navigate to signup page
+
+        signupButton.setOnClickListener{
+            val intent = Intent(this,SignUpPage::class.java)
+            startActivity(intent)
+        }
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.signuppage)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+            insets*/
+
     }
 }
