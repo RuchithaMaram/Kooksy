c35cf0ea8dc1de86bca54b6ee6839bfd51d884be
vaishnavi-chowdary <kommivaishnavi2406@gmail.com>
2024-10-13 22:11:57 -0400

    Linked forgotpassword page

--- app/src/main/java/com/teamfour/kooksy/ui/authentication/ForgotPassword.kt
+++ app/src/main/java/com/teamfour/kooksy/ui/authentication/ForgotPassword.kt
@@ -6,16 +6,28 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.teamfour.kooksy.R
+import android.widget.Button
+import android.content.Intent
 
 class ForgotPassword : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_forgot_password)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.signuppage)) { v, insets ->
+
+        val submitButton = findViewById<Button>(R.id.forgotsubmit)
+
+        submitButton.setOnClickListener {
+            // Submit new password logic here
+            // After submitting, navigate back to login or home
+            val intent = Intent(this, LoginPage::class.java)
+            startActivity(intent)
+        }
+
+    /*    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.signuppage)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
+        } */
     }
 }
