f17cc562705bcad9f0de4bfad1199f490009d959
vaishnavi-chowdary <kommivaishnavi2406@gmail.com>
2024-10-16 22:58:46 -0400

    commit changes - unable to do

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -50,7 +49,9 @@ dependencies {
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.picasso)
     implementation(libs.androidx.activity)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
+
