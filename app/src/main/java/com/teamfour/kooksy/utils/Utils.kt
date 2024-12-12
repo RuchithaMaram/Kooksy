9d42dd651b19233f0ea35182bffaac9bab9f90f1
RuchithaMaram <rmara7@unh.newhaven.edu>
2024-12-11 20:09:27 -0500

    rate

--- app/src/main/java/com/teamfour/kooksy/utils/Utils.kt
+++ app/src/main/java/com/teamfour/kooksy/utils/Utils.kt
@@ -16,10 +16,11 @@ class Utils {
                 recipe_ingredients = document.get("recipe_ingredients") as? List<Map<String, String>> ?: emptyList(),
                 recipe_instructions = document.get("recipe_instructions") as? List<String> ?: emptyList(),
                // createdOn = document.getTimestamp("createdOn"),
-                averageRating = document.getDouble("recipe_rating") ?: 0.0,
+                averageRating = document.getDouble("averageRating") ?: 0.0,
                 totalRating = document.getLong("totalRating")?.toInt() ?: 0,
-                ratingCount = document.getLong("recipe_rating")?.toInt() ?: 0,
+                ratingCount = document.getLong("ratingCount")?.toInt() ?: 0,
                 is_favourite = document.getBoolean("is_favourite") ?: false,
+                ratedBy = document.get("ratedBy") as? List<String> ?: emptyList(),
                 is_rated = document.getBoolean("is_rated") ?: false
             )
         }
